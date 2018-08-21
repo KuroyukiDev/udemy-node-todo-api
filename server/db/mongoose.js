42100708bed8141e5997e39b34e1f1d39fdd4aec
KuroyukiDev <kuroyuki.dev@gmail.com>
2018-08-21 14:09:27 -0700

    Fix mongoose db URI for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<KuroyukiDev>:<Artix0100.>@ds127362.mlab.com:27362/todoapi', {useNewUrlParser: true});
+mongoose.connect('mongodb://KuroyukiDev:Artix0100.@ds127362.mlab.com:27362/todoapi', {useNewUrlParser: true});
 
 module.exports = {mongoose: mongoose};
