fb094d05bc6240a06535250f5721685494e4c55d
KuroyukiDev <kuroyuki.dev@gmail.com>
2018-08-21 13:27:51 -0700

    Add test cases for GET /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,5 +1,6 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 var {Todo} = require('./../models/todo');
@@ -7,9 +8,11 @@ var {User} = require('./../models/user');
 
 const todos = [
   {
+    _id: new ObjectID(),
     text: 'First test todo'
   },
   {
+    _id: new ObjectID(),
     text: 'Second test todo'
   }
 ];
@@ -21,65 +24,104 @@ beforeEach((done) => {
 });
 
 
-describe('POST /todos', () => {
-  it('should create a new todo', (done) => {
-    var text = 'Test todo text';
+describe('/todo route', () => {
 
-    request(app)
-      .post('/todos')
-      .send({text})
-      .expect(200)
-      .expect((res) => {
-        expect(res.body.text).toBe(text);
-      })
-      .end((err, res) => {
-        if (err) {
-          return done(err);
-        }
+  describe('POST /todos', () => {
 
-        Todo.find({text}).then((todos) => {
-          expect(todos.length).toBe(1);
-          expect(todos[0].text).toBe(text);
-          done();
-        }).catch((e) => done(e));
+    it('should create a new todo', (done) => {
+      var text = 'Test todo text';
 
-      });
-  });
+      request(app)
+        .post('/todos')
+        .send({text})
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.text).toBe(text);
+        })
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
 
-  it('should not create todo with invalid body data', (done) => {
+          Todo.find({text}).then((todos) => {
+            expect(todos.length).toBe(1);
+            expect(todos[0].text).toBe(text);
+            done();
+          }).catch((e) => done(e));
 
-    request(app)
-      .post('/todos')
-      .send({})
-      .expect(400)
-      .end((err, res) => {
-        if (err) {
-          return done(err);
-        }
+        });
+    });
 
-        Todo.find({}).then((todos) => {
-          expect(todos.length).toBe(2);
-          done();
-        }).catch((e) => done(e));
-      });
+    it('should not create todo with invalid body data', (done) => {
 
-  });
+      request(app)
+        .post('/todos')
+        .send({})
+        .expect(400)
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
 
-});
+          Todo.find({}).then((todos) => {
+            expect(todos.length).toBe(2);
+            done();
+          }).catch((e) => done(e));
+        });
 
-describe('GET /todos', () => {
+    });
 
-  it('should get all todos', (done) => {
-
-    request(app)
-      .get('/todos')
-      .expect(200)
-      .expect((res) => {
-        expect(res.body.todos.length).toBe(2);
-      })
-      .end(done);
-
-  });
+  }); // POST /todos block
 
 
-});
+  describe('GET /todos', () => {
+
+    it('should get all todos', (done) => {
+
+      request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.todos.length).toBe(2);
+        })
+        .end(done);
+
+    });
+  }); // GET /todos block
+
+  describe('GET /todos/:id', () => {
+    it('should return todo doc', (done) => {
+      request(app)
+        .get(`/todos/${todos[0]._id.toHexString()}`)
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.todo.text).toBe(todos[0].text);
+        })
+        .end(done);
+    });
+  }); // GET /todos/:id block
+
+  describe('GET /todos/:id 404 error tests', () => {
+
+    it('should return 404 if todo not found', (done) => {
+      var hexId = new ObjectID().toHexString();
+
+      request(app)
+        .get(`/todo/${hexId}`)
+        .expect(404)
+        .end(done);
+
+    });
+
+    it('should return 404 for non-obj IDs', (done) => {
+
+      request(app)
+        .get('/todos/123abc')
+        .expect(404)
+        .end(done)
+
+    });
+
+  }); // GET /todos/:id 404 error tests block
+
+}); //  /todo route block
